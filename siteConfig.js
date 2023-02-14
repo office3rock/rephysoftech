efcc121e546990f16cd75494be87f859adf331f7
office3rock <105702924+office3rock@users.noreply.github.com>
2023-02-14 17:32:05 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -11,7 +11,176 @@ module.exports = {
   shareImageHeight: 523, // Change to the height of your default share image
 
   shortTitle: "sitespro99.com", // Used for App manifest e.g. Mobile Home Screen
+  siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manmodule.exports = {
+  siteUrl: "https://svbajas.com", // Site domain. Do not include a trailing slash!
+
+  postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+
+  siteTitleMeta: "svbajas.com", // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta:
+    "svbajas.com", // This allows an alternative site description for meta data for pages.
+
+  shareImageWidth: 1000, // Change to the width of your default share image
+  shareImageHeight: 523, // Change to the height of your default share image
+
+  shortTitle: "svbajas.com", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
+  backgroundColor: "#e9e9e9", // Used for Offline Manifest
+  themeColor: "#15171A", // Used for Offline Manifest
+  apiUrl: "https://wordpress.theasdfghjkl.com",
+  subscribeWidget: {
+    visible: false,
+    title: "svbajas.com",
+    helpText: "הישארו מעודכנים",
+    successMessage: "תודה ההודעה נשלחה בהצלחה",
+  },
+  header: {
+    navigation: [
+      {
+        label: "דף הבית",
+        url: "https://svbajas.com/",
+      },
+      {
+        label: "יצירת קשר",
+        url: "https://svbajas.com/contact",
+      },
+    ],
+  },
+  footer: {
+    copyright: "כל הזכויות שמורות ",
+    navigation: [
+      {
+        label: "דף הבית",
+        url: "https://svbajas.com/",
+      },
+      {
+        label: "Sitemap",
+        url: "https://svbajas.com/sitemap.xml",
+      },
+      
+      {
+        label: "יצירת קשר",
+        url: "https://svbajas.com/contact",
+      },
+     
+    ],
+  },
+  socialLinks: {
+    twitter: "",
+    facebook: "",
+    instagram: "",
+    linkedin: "",
+    github: "",
+    pinterest: "",
+    youtube: "",
+    dribbble: "",
+    behance: "",
+    externalLink: "",
+    whatsapp: "",
+  },
+  contactWidget: {
+    title: "Contact Built with Draftbox",
+    successMessage: "We’ll get in touch with you soon.",
+  },
+  metadata: {
+    title: "svbajas.com",
+    description:
+      "svbajas.com",
+  },
+  twitterCard: {
+    title: "",
+    description:
+      "",
+    imageUrl: "twitterImage.png",
+    username: "@DraftboxHQ",
+  },
+  facebookCard: {
+    title: "svbajas.com",
+    description:
+      "Lightning fast, 1t on Gatsby.",
+    imageUrl: "facebookImage.png",
+    appId: "2391725224459953",
+  },
+  siteTitle: "svbajas.com",
+  siteDescription:
+    "svbajas.com",
+  language: "he",
+  logoUrl: "",
+  iconUrl: "",
+  coverUrl: "cover.jpg",
+  alternateLogoUrl: "",
+  themeConfig: {
+    variables: [
+      {
+        varName: "--primary-color",
+        value: `#2b6cb0`,
+      },
+      {
+        varName: "--primary-color-active",
+        value: `#2a4365`,
+      },
+      {
+        varName: "--primary-color-light",
+        value: `#bee3f8`,
+      },
+      {
+        varName: "--sans-font",
+        value: `"Assistant"`,
+      },
+      {
+        varName: "--sans-font-normal",
+        value: `400`,
+      },
+      {
+        varName: "--sans-font-medium",
+        value: `500`,
+      },
+      {
+        varName: "--sans-font-semibold",
+        value: `600`,
+      },
+      {
+        varName: "--sans-font-bold",
+        value: `700`,
+      },
+      {
+        varName: "--serif-font",
+        value: `Assistant`,
+      },
+      { varName: "--serif-font-light", value: `400` },
+      { varName: "--serif-font-normal", value: `400` },
+      { varName: "--serif-font-medium", value: `400` },
+      { varName: "--serif-font-bold", value: `700` },
+    ],
+    fonts: [
+      {
+        family: "Assistant",
+        variants: ["400", "500", "600", "700"],
+        //subsets: ['latin']
+        //text: 'Hello'
+        fontDisplay: "swap",
+        strategy: "selfHosted", // 'base64' || 'cdn'
+      },
+            {
+        family: "Assistant",
+        variants: ["400", "500", "600", "700"],
+        //subsets: ['latin']
+        //text: 'Hello'
+        //fontDisplay: "swap",
+        //strategy: "selfHosted", // 'base64' || 'cdn'
+      },
+      {
+        family: "Assistant",
+        variants: ["300", "400", "700"],
+        //subsets: ['latin']
+        //text: 'Hello'
+        fontDisplay: "swap",
+        strategy: "selfHosted", // 'base64' || 'cdn'
+      },
+    ],
+  },
+};
+ifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
